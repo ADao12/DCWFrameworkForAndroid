c15db9a0412e64bd5c71033dd5a14c70905e32e7
adao12 <adao12@qq.com>
2015-07-21 23:00:22 +0800

    add StateView

---
+++ DCWFramework/dcwdianping/src/main/java/com/dcw/app/rating/biz/test/StateViewFragment.java
@@ -0,0 +1,81 @@
+package com.dcw.app.rating.biz.test;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.dcw.app.rating.R;
+import com.dcw.app.rating.biz.MainActivity;
+import com.dcw.app.rating.ui.adapter.BaseFragmentWrapper;
+import com.dcw.app.rating.ui.lib.StateView;
+import com.dcw.framework.view.annotation.InjectLayout;
+import com.dcw.framework.view.annotation.InjectView;
+
+/**
+ * Created by adao12 on 2015/7/21.
+ */
+@InjectLayout(R.layout.fragment_state_view)
+public class StateViewFragment extends BaseFragmentWrapper {
+
+    StateView.ContentState mState;
+
+    @InjectView(R.id.content)
+    private StateView mNGStateView;
+
+    @InjectView(value = R.id.btn_change_state, listeners = View.OnClickListener.class)
+    private Button mChangeState;
+
+    private int index = 0;
+
+    @Override
+    public Class getHostActivity() {
+        return MainActivity.class;
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public void initUI() {
+        mState = mNGStateView.getState();
+        mNGStateView.setOnEmptyViewBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastMsg("empty view button click!");
+            }
+        });
+        setState(StateView.ContentState.ERROR_NETWORK);
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+
+    public void setState(StateView.ContentState state) {
+        mNGStateView.setState(state);
+        mState = state;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_change_state:
+                index ++;
+                if (index >= StateView.ContentState.values().length) {
+                    index = 0;
+                }
+                setState(StateView.ContentState.values()[index]);
+                break;
+            default:
+
+                break;
+        }
+    }
+
+    private void toastMsg(String msg) {
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+    }
+}
